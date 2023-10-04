88c9cc49f4f6dd16e983769d888899ac3d840b07
Guillermo Garay <114153361+ggarjai@users.noreply.github.com>
2023-10-04 23:33:03 +0200

    Delete UF2/EJ2 directory

--- UF2/EJ2/codigo.js
+++
@@ -1,7 +0,0 @@
-let bienvenida = "Hola mundo\n"
-let mensaje = "Qué fácil es incluir \'Comillas simples\' y \"Comillas dobles\" "
-
-console.log(bienvenida)
-console.log(mensaje)
-//Comentario para explicar el funcionamiento
-
